563fd198a344c3d203a09d4f74611d84b859f572
David Rihtarsic <david.rihtarsic@gmail.com>
2023-06-28 10:25:44 +0200

    testni program spremenjen tako, da se da meriti tudi A0

--- examples/FreeWheely/r000_Test_vmesnika/r000_Test_vmesnika.ino
+++ examples/FreeWheely/r000_Test_vmesnika/r000_Test_vmesnika.ino
@@ -4,9 +4,10 @@
 // 2. Preskusite delovanje vmesnika tako, da pritiskate na tipki C4 ali C5...
 // link:https://sites.google.com/site/drtirobotika/izobrazevanje/arduino/robotika-v-tehniki/freewheely-rob#TOC-Priklju-itev-vmesnika
 ////////////////////////////////////////////////////////
-#include "RobDuino.h"         
-Button TipkaC4(pC4);  
-Button TipkaC5(pC5);        
+#include "RobDuino.h"
+Button TipkaC4(pC4);
+Button TipkaC5(pC5);
+bool test_tipk = 1;
 int i = 1;
 void setup()
 {
@@ -15,15 +16,15 @@ void setup()
 
 void loop()
 {
-  if (TipkaC4.down()){
-    i = i<<1;
+  if (TipkaC4.down()) i = i<<1;
+  if (TipkaC5.down()) i = i>>1;
+  if (TipkaC4.down() && TipkaC5.down()) test_tipk = !test_tipk;
+  if (test_tipk){
     if (i>255) i = 1;
-  }
-  if (TipkaC5.down()){
-    i = i>>1;
     if (i<1) i = 128;
+    PORTD = i;
+  }else{
+    PORTD = analogRead(A0) >> 2;
   }
-  PORTD = i;
-  delay(50);
+  delay(200);
 }
-
